aa0e347d169fe24c7bdf26d742ad809519d924a8
dasom222g <dasom228@gmail.com>
2024-01-02 22:37:00 +0900

    Add action

---
+++ src/modules/counter.ts
@@ -0,0 +1,10 @@
+// action type 정의
+const INCREASE = 'counter/INCREASE' as const
+const DECREASE = 'counter/DECREASE' as const
+const INCREASE_BY = 'counter/INCREASE_BY' as const
+
+// action 생성자 함수
+// string타입이 아닌 실제 값을 타입으로 지정해서 타입 범위 좁힘`
+export const increase = () => ({ type: INCREASE })
+export const decrease = () => ({ type: DECREASE })
+export const increase_by = (diff: number) => ({ type: INCREASE_BY, payload: diff })
