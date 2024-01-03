578aa247b4a0d209ca6e56dea1f1878993877fb3
dasom222g <dasom228@gmail.com>
2024-01-03 23:58:46 +0900

    Fix diff

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -7,4 +7,4 @@ const INCREASE_BY = 'counter/INCREASE_BY' as const
 // string타입이 아닌 실제 값을 타입으로 지정해서 타입 범위 좁힘`
 export const increase = () => ({ type: INCREASE })
 export const decrease = () => ({ type: DECREASE })
-export const increase_by = (diff: number) => ({ type: INCREASE_BY, payload: diff })
+export const increase_by = (step: number) => ({ type: INCREASE_BY, payload: step })
